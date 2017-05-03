6d0719a3995fe323fe6e564320a183f5234552b2
bentonwong <bentonwong@gmail.com>
2017-05-03 12:57:49 -0700

    add accounts receiveable and clients with outstanding bills

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -8,7 +8,12 @@ class TimeEntry < ApplicationRecord
   end
 
   def self.accounts_receivable
-    where("paid = ? AND billable = ?", false, false).sum {|time_entry| time_entry.cost}
+    where("paid = ? AND billable = ?", false, true).sum {|time_entry| time_entry.cost}
+  end
+
+  def self.outstanding_clients
+    client_ids = Matter.all.map {|matter| matter.client_id if matter.time_entries.where("paid = ? AND billable = ?", false, true)}
+    client_ids.uniq.map {|client_id| Client.find_by(id: client_id)}
   end
 
 end
