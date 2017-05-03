0becd1b9ef549986ad7c5ad0d8cb260e5a995ea5
bentonwong <bentonwong@gmail.com>
2017-05-03 10:32:42 -0700

    fix bug with calculate invoice method

--- app/models/matter.rb
+++ app/models/matter.rb
@@ -4,9 +4,8 @@ class Matter < ApplicationRecord
   has_many :time_entries
 
   def calculate_invoice
-    self.time_entries.collect do |time_entry|
-      time_entry[:rate] * time_entry[:duration] if time_entry[:billable]
-    end.sum
+    billable_entries = self.time_entries.select {|time_entry| time_entry[:billable] && !time_entry[:paid]}
+    billable_entries.collect {|time_entry| time_entry[:rate] * time_entry[:duration]}.sum
   end
 
 end
