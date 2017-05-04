79229cd2fd4cbbd0b24855f607f63e6c64f3a5ee
bentonwong <bentonwong@gmail.com>
2017-05-04 09:47:08 -0700

    add controller actions to Expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,10 @@
 class ExpensesController < ApplicationController
   def index
+    @expenses = Expense.all
   end
 
   def show
+    @expense = Expense.find_by(id: params[:id])
   end
 
   def destroy
