430cc10aff6b1d346dde177cfb16b39dc8a16047
bentonwong <bentonwong@gmail.com>
2017-05-04 18:09:02 -0700

    add email validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,8 @@ class Client < ApplicationRecord
   has_many :matters
   has_many :lawyers, through: :matters
   validates :name, :address, :phone, :email, presence: :true
+  validates :email, uniqueness: :true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   def total_outstanding_balance
     matters.sum {|matter| matter.calculate_matter_invoice}
--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -3,6 +3,9 @@ class Lawyer < ApplicationRecord
   has_many :clients, through: :matters
   has_many :time_entries
   validates :name, :email, :rate, presence: :true
+  validates :email, uniqueness: :true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
   #validates :password, length: { minimum: 8 }, allow_nil: true
   #has_secure_password
 
