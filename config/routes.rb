012e8141602892490251ce9c719adc1363b5343d
bentonwong <bentonwong@gmail.com>
2017-05-09 13:08:46 -0700

    clean up files

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "clients/:id/invoice" => "clients#invoice", as: :client_invoice
   get "matters/:id/time_entries" =>  "matters#time_entries", as: :matter_time_entries
   get "matters/:id/time_entries/new" => "time_entries#new", as: :new_matter_time_entry
+  
   get '/signin', to: 'sessions#new', as: 'signin'
   get '/signup', to: 'lawyers#new', as: 'signup'
   get '/logout', to: 'sessions#destroy', as: 'logout'
