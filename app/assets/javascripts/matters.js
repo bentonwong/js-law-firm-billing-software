6e91ccf3040af76fc961c60be801747dddcb1880
bentonwong <bentonwong@gmail.com>
2017-06-10 23:22:01 -0700

    build js/jQuery for lawyer show page

--- app/assets/javascripts/matters.js
+++ app/assets/javascripts/matters.js
@@ -1,5 +1,5 @@
 function addToMattersShowTable(response) {
-  var tr_item = undefined
+  var tr_item = ""
   tr_item += "<tr>"
   tr_item += "<td>" + response.date + "</td>"
   tr_item += "<td><a href='/matters/" + response.matter_id + "/time_entries/" + response.id + "'>" + response.description + "</a></td>"
@@ -9,7 +9,22 @@ function addToMattersShowTable(response) {
   $('#show_matter_time_entries tbody').append(tr_item)
 }
 
-for (var i=0; i < response.matters.length; i++){
+function addToLawyersShowTable(response) {
+  const response_matters = response.matters
+  for (var i=0; i < response_matters.length; i++) {
+    var matter_client_id = response_matters[i].client_id
+    var client = response.clients.filter(function(o){
+      return o.id === matter_client_id;
+    });
+    var tr_item = ""
+    tr_item += "<tr>"
+    tr_item += "<td>" + response_matters[i].id + "</td>"
+    tr_item += "<td><a href='/matters/" + response_matters[i].id + "'>" + response_matters[i].name + "</a></td>"
+    tr_item += "<td><a href='/clients/" + response_matters[i].client_id + "'>" + client[0].name + "</a></td>"
+    tr_item += "</tr>"
+    $('#show_lawyer_matters').append(tr_item)
+  }
+}
 
 $(function(){
   if(!$('div#turn_off_event_handler').length){
@@ -68,8 +83,8 @@ $(function(){
       });
     });
   }
-  if (!!$('ol#show_lawyer_matters').length) {
-    $('ol#show_lawyer_matters').ready(function(e){
+  if (!!$('table#show_lawyer_matters').length) {
+    $('table#show_lawyer_matters').ready(function(e){
       const lawyer_id = $("#lawyer_show_header").attr("lawyer-id");
       $.ajax({
         url: '/lawyers/' + lawyer_id,
@@ -77,7 +92,7 @@ $(function(){
         dataType: "JSON",
         success: function(response){
           const table_header = "<tr><th>Matter ID</th><th>Matter</th><th>Client</th></tr>"
-          $('ol#show_lawyer_matters').append(table_header)
+          $('table#show_lawyer_matters').append(table_header)
           addToLawyersShowTable(response)
         }
       });
