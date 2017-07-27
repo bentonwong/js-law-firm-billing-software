fc4696ca7ece0757486e462eca82612b27ec1ba0
bentonwong <bentonwong@gmail.com>
2017-07-27 09:20:07 -0700

    update lawyer table uid data type

--- db/migrate/20170727083103_create_lawyers.rb
+++ db/migrate/20170727083103_create_lawyers.rb
@@ -7,7 +7,7 @@ class CreateLawyers < ActiveRecord::Migration[5.0]
       t.string :provider
       t.string :oauth_token
       t.datetime :oauth_expires_at
-      t.integer :uid, :limit => 16
+      t.string :uid
       t.decimal :rate, :default => 0
     end
   end
